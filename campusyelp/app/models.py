bd0ee89b114a79f950be0d01dde9c5423ba55672
Brett Konold <brett@wifi-131-179-15-197.host.ucla.edu>
2014-02-25 20:35:32 -0800

    Define models

--- campusyelp/app/models.py
+++ campusyelp/app/models.py
@@ -2,19 +2,31 @@ from django.db import models
 
 # Create your models here.
 
-class Menu(models.Model):
-	day = models.DateField(primary_key=True)
-	food_items = models.ManyToManyField(Food)
 
 class Food(models.Model):
 	title = models.CharField(max_length=100, unique=True)
 
+	def __unicode__(self):
+		return "Food: " + self.title
+class Menu(models.Model):
+	day = models.DateField(primary_key=True)
+	food_items = models.ManyToManyField(Food)
+
+	def __unicode__(self):
+		return "Food Items: [" + food_items.join(" ") + "]"
+
 class Review(models.Model):
 	title = models.CharField(max_length=100)
 	content = models.CharField(max_length=500)
-	rating = models.IntField()
+	rating = models.IntegerField()
 	food_item = models.ForeignKey(Food)
+
+	def __unicode__(self):
+		return "Review: " + self.title
 
 class Image(models.Model):
 	path = models.CharField(max_length=200)
 	food_item = models.ForeignKey(Food)
+
+	def __unicode__(self):
+		return "Path: " + self.path + "\n" + "Food: " + self.food_item
