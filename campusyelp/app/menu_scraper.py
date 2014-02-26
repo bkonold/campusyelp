ebbc017d4036b4ada9663033d18b94232f29b902
Brett Konold <brett@wifi-131-179-15-197.host.ucla.edu>
2014-02-25 21:08:03 -0800

    Adding menu scraper

---
+++ campusyelp/app/menu_scraper.py
@@ -0,0 +1,60 @@
+from bs4 import BeautifulSoup
+import urllib2
+import re
+
+# get html
+try: 
+	html = urllib2.urlopen("http://menu.ha.ucla.edu/foodpro/default.asp").read()
+except urllib2.URLError:
+	try:
+		f = open("menuHTML.html", 'r')
+		html = f.read()
+	except:
+		print "no connection or local HTML menu"
+		exit()
+
+# convert to beautitfulsoup object
+soup = BeautifulSoup(html)
+
+# extract text from html
+text = soup.get_text()
+
+# remove extraneous characters(tabs, weird spaces, extra new lines, etc)
+notabs = re.sub("\t+" , "\n", text)
+space = re.sub("\xa0" , " ", notabs)
+withSymbol = re.sub("\nw/|w/\n", "w/", space)
+extraneous = re.sub("(Full Menu.*\n)|(Click.*\n)" , "\n", withSymbol)
+emptyLines = re.sub("\n+ +| +\n", "\n", extraneous)
+final = re.sub("[\n\r]{2,}", "\n\n", emptyLines)
+
+
+lunchStart = final.find("Lunch Menu")
+dinnerStart = final.find("Dinner Menu")
+dinnerEnd = final.find("Back to Top")
+
+# get each part
+lunchText = final[lunchStart:dinnerStart].strip()
+dinnerText = final[dinnerStart:dinnerEnd].strip()
+
+#create list with menuData
+dinnerList = [i.split("\n") for i in dinnerText.encode('ascii', 'ignore').split("\n\n")]
+lunchList = [i.split("\n") for i in lunchText.encode('ascii', 'ignore').split("\n\n")]
+
+diningHalls = ["Covel", "De Neve", "FEAST at Rieber", "Bruin Plate"]
+
+# get positions of dining hall list
+lunchPos = []
+for it, i in enumerate(lunchList):
+	for name in i:
+		if (name in diningHalls):
+			lunchPos.append(it)
+			break
+
+lunchSet1 = lunchList[lunchPos[0]:lunchPos[1]]
+lunchSet2 = lunchList[lunchPos[1]:]
+
+covelLunch = [i for it, i in enumerate(lunchSet1[1:]) if it%2 == 0]
+deNeveLunch = [i for it, i in enumerate(lunchSet1[1:]) if it%2 == 1]
+feastLunch = [i for it, i in enumerate(lunchSet2[1:]) if it%2 == 0]
+bpLunch = [i for it, i in enumerate(lunchSet2[1:]) if it%2 == 1]
+
