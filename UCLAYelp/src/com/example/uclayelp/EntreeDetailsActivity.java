2238e5216e103c1e8fbb0b4bb384a57c881731f3
Abby Sywe <abby@Abby-2.local>
2014-03-01 15:55:42 -0800

    no more settings

--- UCLAYelp/src/com/example/uclayelp/EntreeDetailsActivity.java
+++ UCLAYelp/src/com/example/uclayelp/EntreeDetailsActivity.java
@@ -78,12 +78,6 @@ public class EntreeDetailsActivity extends Activity implements OnClickListener {
 		}
 	}
 	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.display_menus, menu);
-		return true;
-	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
