81a52f694c605a1a8554ebbc25e1261bfc6642fe
Brett Konold <brett@Bretts-MacBook.local>
2014-03-01 17:37:38 -0800

    Use MySQL instead of SQLite

--- campusyelp/campusyelp/settings.py
+++ campusyelp/campusyelp/settings.py
@@ -56,10 +56,20 @@ WSGI_APPLICATION = 'campusyelp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'campusyelp',
+        'USER': 'root',
+        'PASSWORD': 'ucla',
+        'HOST': '54.186.3.129',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
