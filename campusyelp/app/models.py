6c1fd39fb4128b4d7141b7955b31a5912d69eb31
Brett Konold <brett@wifi-131-179-15-111.host.ucla.edu>
2014-03-04 20:22:50 -0800

    oops

--- campusyelp/app/models.py
+++ campusyelp/app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Food(models.Model):
 	title = models.CharField(max_length=100, unique=True)
 	rating = models.FloatField()
-	numreviews = models.IntField()
+	numreviews = models.FloatField()
 
 	def __unicode__(self):
 		return self.title
